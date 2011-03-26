993bfac386f4fab615e08399acdc099a9f2e365b
Benjamin Amankwata <benjamin.amankwata@gmail.com>
2011-03-26 11:29:59 -0400

    ffeege

---
+++ src/com/example/googlehelloandroid/GoogleHelloAndroid.java
@@ -0,0 +1,13 @@
+package com.example.googlehelloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleHelloAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
